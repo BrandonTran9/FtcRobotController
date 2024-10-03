55eb4f82912ed17363af124ce9086915e3e49e5b
Oscar <omireles724@icloud.com>
2024-10-02 17:15:22 -0700

    auto
    Signed-off-by: Oscar <omireles724@icloud.com>

---
+++ TeamCode/src/main/java/MainAuto.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class MainAuto extends LinearOpMode {
+    DcMotor fr;
+    DcMotor fl;
+    DcMotor br;
+    DcMotor bl;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        {
+            fr=hardwareMap.dcMotor.get("fr");
+            fl=hardwareMap.dcMotor.get("fl");
+            br =hardwareMap.dcMotor.get("rf");
+            bl=hardwareMap.dcMotor.get("bl");
+            waitForStart();
+            fr.setPower(-0.35);
+            fl.setPower(-0.35);
+            br.setPower(0.35);
+            bl.setPower(0.35);
+            sleep(1500);
+
+
+        }
+    }
+}
