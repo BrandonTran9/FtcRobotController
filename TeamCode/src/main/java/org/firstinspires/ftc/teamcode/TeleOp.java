b5d27b965e6f0763428ecdd22c03db883489eee0
Unnece55ary5face <166060662+Unnece55ary5face@users.noreply.github.com>
2024-10-05 11:58:32 -0700

    Updated TeleOp.java
    now it has the correct names and stuff but im too lazy to write it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -6,74 +6,25 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
-public class TeleOp extends OpMode {
-    DcMotor lf, lb, rf, rb, intakeMotor, hangingMotor;
-    CRServo drone;
+public class teleop extends OpMode {
+    DcMotor arm, hang, lift, FR, FL, BL, BR;
+    CRServo SL,SR,Srotate;
     @Override
-    public void init() {
-        lf = hardwareMap.dcMotor.get("lf");
-        lb = hardwareMap.dcMotor.get("lb");
-        rf = hardwareMap.dcMotor.get("rf");
-        rb = hardwareMap.dcMotor.get("rb");
-        intakeMotor = hardwareMap.dcMotor.get("intake");
-        hangingMotor = hardwareMap.dcMotor.get("hanging");
-        drone = hardwareMap.crservo.get("drone");
-        lf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        lb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    public void init (){
+        BR = hardwareMap.dcMotor.get("BR");
+        BL = hardwareMap.dcMotor.get("BL");
+        FR = hardwareMap.dcMotor.get("FR");
+        FL = hardwareMap.dcMotor.get("FL");
+        arm = hardwareMap.dcMotor.get("arm");
+        hang = hardwareMap.dcMotor.get("hang");
+        lift = hardwareMap.dcMotor.get("lift");
+        SL = hardwareMap.crservo.get("SL");
+        SR = hardwareMap.crservo.get("SR");
+        Srotate = hardwareMap.crservo.get("Srotate");
     }
     @Override
     public void loop() {
-        if (Math.abs(-gamepad1.right_stick_y) > .2) {
-            rf.setPower(-gamepad1.right_stick_y * 1);
-            rb.setPower(-gamepad1.right_stick_y * 1);
-        } else {
-            rb.setPower(0);
-            rf.setPower(0);
-        }
-        if (Math.abs(gamepad1.left_stick_y) > .2) {
-            lf.setPower(gamepad1.left_stick_y * 1);
-            lb.setPower(-gamepad1.left_stick_y * 1);
-        } else {
-            lf.setPower(0);
-            lb.setPower(0);
-        }
-        if (gamepad2.a) {
-            intakeMotor.setPower(1);
-        } else {
-            intakeMotor.setPower(0);
-        }
-
-        if (gamepad2.b) {
-            intakeMotor.setPower(-1);
-        } else {
-            intakeMotor.setPower(0);
-        }
-
-        if (gamepad2.x) {
-            drone.setPower(1);
-        } else {
-            drone.setPower(0);
-        }
-
-        if (gamepad2.y) {
-            drone.setPower(-1);
-        } else {
-            drone.setPower(0);
-        }
-
-        if (gamepad2.dpad_up) {
-            hangingMotor.setPower(1);
-        } else {
-            hangingMotor.setPower(0);
-        }
-
-        if (gamepad2.dpad_down) {
-            hangingMotor.setPower(-1);
-        } else {
-            hangingMotor.setPower(0);
-        }
 
     }
 }
+
