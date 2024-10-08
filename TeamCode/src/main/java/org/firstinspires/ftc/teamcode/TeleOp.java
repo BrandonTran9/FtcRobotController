e84e54b0740237b12be4f9a1e8fb3bd3c0bf4680
Unnece55ary5face <166060662+Unnece55ary5face@users.noreply.github.com>
2024-10-08 16:31:24 -0700

    Update TeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -8,9 +8,10 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp
 public class teleop extends OpMode {
     DcMotor arm, hang, lift, FR, FL, BL, BR;
-    CRServo SL,SR,Srotate;
+    CRServo SL, SR, Srotate;
+
     @Override
-    public void init (){
+    public void init() {
         BR = hardwareMap.dcMotor.get("BR");
         BL = hardwareMap.dcMotor.get("BL");
         FR = hardwareMap.dcMotor.get("FR");
@@ -22,9 +23,38 @@ public class teleop extends OpMode {
         SR = hardwareMap.crservo.get("SR");
         Srotate = hardwareMap.crservo.get("Srotate");
     }
+
     @Override
     public void loop() {
-
+        if (Math.abs(gamepad1.right_stick_y) > .2) {
+            FR.setPower(gamepad1.right_stick_y * 1);
+            BR.setPower(gamepad1.right_stick_y * 1);
+        } else {
+            BR.setPower(0);
+            FR.setPower(0);
+        }
+        if (Math.abs(gamepad1.left_stick_y) > .2) {
+            FL.setPower(gamepad1.left_stick_y * 1);
+            BR.setPower(gamepad1.left_stick_y * 1);
+        } else {
+            BL.setPower(0);
+            FL.setPower(0);
+        }
+        if (gamepad1.right_bumper) {
+            FR.setPower(1);
+            BR.setPower(-1);
+            BL.setPower(-1);
+            FL.setPower(1);
+        } else {
+            FR.setPower(0);
+            BR.setPower(0);
+            BL.setPower(0);
+            FL.setPower(0);
+        }
+        if (Math.abs(gamepad2.right_stick_y) > .2) {
+            arm.setPower(1);
+        } else {
+            arm.setPower(0);
+        }
     }
 }
-
