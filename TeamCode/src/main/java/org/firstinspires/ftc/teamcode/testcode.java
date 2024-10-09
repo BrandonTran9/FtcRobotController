046389354f726e7225ba0fe344d5fa46a1a1c864
Oscar <omireles724@icloud.com>
2024-10-08 17:01:38 -0700

    test
    Signed-off-by: Oscar <omireles724@icloud.com>

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testcode.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class testcode extends TeleOp{
+    DcMotor hanging_arm, hanging;
+    @Override
+    public void init() {
+        hanging_arm = hardwareMap.dcMotor.get("hanging_arm");
+        hanging = hardwareMap.dcMotor.get("hanging");
+    }
+    @Override
+    public void loop() {
+
+        if (gamepad1.dpad_up) {
+            hanging_arm.setPower(1);
+        } else {
+            hanging_arm.setPower(0);
+        }
+
+        if (gamepad1.dpad_down) {
+            hanging.setPower(-1);
+        } else {
+            hanging.setPower(0);
+    }
+}
