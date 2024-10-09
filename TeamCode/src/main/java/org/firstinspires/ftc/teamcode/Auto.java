3f73570c7243fe4614b056c71384d06f3cfe3ec5
BrandonTran9 <327016@my.puhsd.org>
2024-10-09 15:47:02 -0700

    most recent update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -4,22 +4,22 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class Auto extends LinearOpMode {
-    DcMotor fr;
-    DcMotor fl;
-    DcMotor br;
-    DcMotor bl;
+    DcMotor FR;
+    DcMotor FL;
+    DcMotor BR;
+    DcMotor BL;
     @Override
     public void runOpMode() throws InterruptedException {
         {
-            fr=hardwareMap.dcMotor.get("fr");
-            fl=hardwareMap.dcMotor.get("fl");
-            br =hardwareMap.dcMotor.get("rf");
-            bl=hardwareMap.dcMotor.get("bl");
+            FR =hardwareMap.dcMotor.get("FR");
+            FL =hardwareMap.dcMotor.get("FL");
+            BR =hardwareMap.dcMotor.get("BR");
+            BL =hardwareMap.dcMotor.get("BL");
             waitForStart();
-            fr.setPower(-0.35);
-            fl.setPower(-0.35);
-            br.setPower(0.35);
-            bl.setPower(0.35);
+            FR.setPower(-0.35);
+            FL.setPower(-0.35);
+            BR.setPower(0.35);
+            BL.setPower(0.35);
             sleep(1500);
 
 
