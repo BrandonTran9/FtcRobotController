a4eda1d1ef369d6bcca821d81fa61d2315b4be80
BrandonTran9 <327016@my.puhsd.org>
2024-10-15 16:26:31 -0700

    new auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -1,13 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class Auto extends LinearOpMode {
-    DcMotor FR;
-    DcMotor FL;
-    DcMotor BR;
-    DcMotor BL;
+    DcMotor arm, lift, FR, FL, BL, BR,arm2,hang;
+    CRServo SL, SR, Srotate;
     @Override
     public void runOpMode() throws InterruptedException {
         {
