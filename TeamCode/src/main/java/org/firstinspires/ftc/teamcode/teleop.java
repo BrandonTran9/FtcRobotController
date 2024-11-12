d6410ef0b41de0c24bed9b730b838ea4f3d0eb34
BrandonTran9 <144183256+BrandonTran9@users.noreply.github.com>
2024-11-12 10:31:52 -0800

    Update teleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
@@ -9,7 +9,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class teleop extends OpMode {
     static final double     COUNTS_PER_MOTOR_REV    = 435 ;    // eg: TETRIX Motor Encoder
     static final double     DRIVE_GEAR_REDUCTION    = 1 ;     // No External Gearing.
-    static final double     WHEEL_DIAMETER_INCHES   = 7.55906 ;     // For figuring circumference
+    static final double     WHEEL_DIAMETER_INCHES   = 4.09449 ;     // For figuring circumference
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
             (WHEEL_DIAMETER_INCHES * 3.1415);
 
